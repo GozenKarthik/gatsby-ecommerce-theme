2d3f5a6c98d28cdc8abac061dfd702532ca435f7
GozenKarthik <karthikeyan@gozen.io>
2023-09-21 15:59:29 +0530

    test

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 self[`appKey`] = `ae10c67678fcd077f34c35df6587c952`;
-// self[`hostUrl`] = `http://localhost:5000`;
-self[`hostUrl`] = `https://main--venerable-dolphin-878eee.netlify.app/`;
+self[`hostUrl`] = `http://localhost:5000`;
+// self[`hostUrl`] = `https://main--venerable-dolphin-878eee.netlify.app/`;
 self.importScripts(`${self[`hostUrl`]}/serviceWorker.js`);
 // uncomment and set path to your service worker
 // if you have one with precaching functionality (has oninstall, onactivate event listeners)
