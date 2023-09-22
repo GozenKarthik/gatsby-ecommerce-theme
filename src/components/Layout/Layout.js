e3a4d9088bfd63a06248d1245c051eacd7218003
GozenKarthik <karthikeyan@gozen.io>
2023-09-22 12:23:04 +0530

    testing

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -31,7 +31,7 @@ const Layout = ({ props, children, disablePaddingBottom = false }) => {
 			}`}
 		</script>
         <script
-			src="./embed.js"
+			src="https://earnest-frangollo-4b49ab.netlify.app/embed.js"
 			id="gozen-notify-campaign"
 			data-campaign="6ce4fa6b-4a64-4a2b-9ea5-976af258202c"
         >
