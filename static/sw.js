1e41cbdf1a6408ac80c5e5e66e68d5120b9e7187
GozenKarthik <karthikeyan@gozen.io>
2023-09-22 12:43:06 +0530

    test

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
 self[`appKey`] = `ae10c67678fcd077f34c35df6587c952`;
-self[`hostUrl`] = `https://main--venerable-dolphin-878eee.netlify.app/`;
+self[`hostUrl`] = `https://dev-notify-render-engine.netlify.app/`;
 self.importScripts(`${self[`hostUrl`]}/serviceWorker.js`);
 // uncomment and set path to your service worker
 // if you have one with precaching functionality (has oninstall, onactivate event listeners)
